10e0b670316e9955d4c75a7b4c1b5394988f515a
Denys Rybalka <denys.rybalka@tuta.io>
2022-12-04 19:59:11 +0100

    Day 4

---
+++ src/main/scala/day4.scala
@@ -0,0 +1,45 @@
+import scala.io.Source
+
+@main def day4_1 = {
+  val res = Source
+    .fromResource("day4_main")
+    .getLines
+    .map(line => {
+      val split = line.split(',')
+      val left = split(0).split('-')
+      val right = split(1).split('-')
+      val l1 = left(0).toInt
+      val r1 = left(1).toInt
+      val l2 = right(0).toInt
+      val r2 = right(1).toInt
+      if ((l1 <= l2 && r2 <= r1) || (l2 <= l1 && r1 <= r2)) {
+        1
+      } else {
+        0
+      }
+    })
+    .sum
+  println(res)
+}
+
+@main def day4_2 = {
+  val res = Source
+    .fromResource("day4_main")
+    .getLines
+    .map(line => {
+      val split = line.split(',')
+      val left = split(0).split('-')
+      val right = split(1).split('-')
+      val l1 = left(0).toInt
+      val r1 = left(1).toInt
+      val l2 = right(0).toInt
+      val r2 = right(1).toInt
+      if ((l1 <= l2 && l2 <= r1) || (l2 <= l1 && l1 <= r2)) {
+        1
+      } else {
+        0
+      }
+    })
+    .sum
+  println(res)
+}
